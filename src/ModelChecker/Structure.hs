32c7834ba9d54149992cabf9bac34d19215c106c
Ishan Bhargava <ishantheperson@users.noreply.github.com>
2019-12-09 18:33:23 -0500

    Formatting

--- src/ModelChecker/Structure.hs
+++ src/ModelChecker/Structure.hs
@@ -18,9 +18,9 @@ import Control.Monad.State
 import Control.Monad.Writer 
 
 data Structure a b c sigma = Structure {
-  binOp :: Maybe (DFA a sigma (Succ (Succ Zero))),
+  binOp     :: Maybe (DFA a sigma (Succ (Succ Zero))),
   ternaryOp :: Maybe (DFA b sigma (Succ (Succ (Succ Zero)))),
-  equalOp :: DFA c sigma (Succ (Succ Zero))
+  equalOp   :: DFA c sigma (Succ (Succ Zero))
 }
 
 type Context m = (MonadState (Set String) m, MonadWriter [String] m)
@@ -28,9 +28,10 @@ type Context m = (MonadState (Set String) m, MonadWriter [String] m)
 -- | Takes a structure and a statement, and returns a list of errors encountered
 checkStatement structure (Statement qs m) = 
   execWriter $ execStateT (forM_ qs checkQuant >> checkAST m) Set.empty 
+
   where checkQuant :: Context m => Quantifier -> m () 
         checkQuant s = do 
-          let name = case s of { Forall s -> s; Exists s -> s }
+          let name = case s of Forall s -> s; Exists s -> s 
 
           seen <- gets $ Set.member name 
           when seen $ tell ["variable defined multiple times: '" ++ name ++ "'"]
@@ -47,13 +48,13 @@ checkStatement structure (Statement qs m) =
           And m1 m2 -> checkAST m1 >> checkAST m2 
           RelatedTo a b -> case binOp structure of 
             Nothing -> tell ["binary operation not supported"]
-            Just _ -> checkVar a >> checkVar b 
+            Just _  -> forM_ [a, b] checkVar 
 
           TernaryOp a b c -> case ternaryOp structure of 
             Nothing -> tell ["ternary operation not supported"]
-            Just _ -> forM_ [a, b, c] checkVar
+            Just _  -> forM_ [a, b, c] checkVar
 
-          Equals a b -> checkVar a >> checkVar b 
+          Equals a b -> forM_ [a, b] checkVar 
 
 -- | Theory of Presburger arithmetic, or just arithmetic with only addition. 
 -- 
